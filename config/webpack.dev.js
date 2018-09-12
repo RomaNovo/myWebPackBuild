539aefcba713253b07329589e9def842d84ab334
RomaNovo <roma_1985_roma@mail.ru>
2018-09-12 14:57:14 +0300

    express1

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,4 +1,5 @@
-const path = require("path")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
 	entry: {
@@ -12,7 +13,11 @@ module.exports = {
 	},
 	devServer: {
 		contentBase: "dist",
-		overlay: true
+		overlay: true,
+		hot: true,
+		stats: {
+			color: true
+		}
 	},
 	module: {
 		rules: [
@@ -63,6 +68,9 @@ module.exports = {
 				}
 			}
 		]
-	}
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin()
+	]
 
 }
