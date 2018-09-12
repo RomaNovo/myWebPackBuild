e19bedb8494616bee222c1087115ffcc15c126a5
RomaNovo <roma_1985_roma@mail.ru>
2018-09-12 15:29:32 +0300

    final express

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 require("babel-runtime/regenerator");
-require("webpack-hot-middleware/client");
+require("webpack-hot-middleware/client?reload=true");
 require("./main.css");
 require("./index.html");
+
+alert('hi')
+
+
+
+
+
 
 
